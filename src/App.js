efc9faeecee3089b8f652274fa1c4198123a67a4
AlinaTaoRao <raotao77@outlook.com>
2022-04-25 12:51:05 +0200

    edited cards component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import {Cards} from './components/cards/Cards.jsx';
 
 function App() {
   return (
     <div className="App">
-      
+      <Cards/>
     </div>
   );
 }
---
+++ src/components/cards/Cards.jsx
@@ -0,0 +1,40 @@
+import "./styles.css";
+import { data } from "../../data.js";
+
+export const Cards = () => {
+  return (
+    <div className="cards-container">
+      {data.map((movie, index) => (
+        <div key={index} className="movie-card">
+          <h2>{movie.Title}</h2>
+          <button className="movie-poster">
+            <img src={movie.Poster} alt={`${movie.Title} poster`} />
+          </button>
+          <div className="info-bar">
+            <i className="public-time">
+              <svg
+                className="MuiSvgIcon-root MuiSvgIcon-fontSizeSmall"
+                focusable="false"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+              >
+                <path d="M20 3h-1V1h-2v2H7V1H5v2H4c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 18H4V8h16v13z"></path>
+              </svg>
+              {movie.Year}
+            </i>
+            <button className="like-icon">
+              <svg
+                className="MuiSvgIcon-root"
+                focusable="false"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+              >
+                <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
