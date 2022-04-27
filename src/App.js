7fe1fd338e92e137265d65faa8b25a454635bbb0
AlinaTaoRao <raotao77@outlook.com>
2022-04-27 11:31:53 +0200

    added details feature way 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,76 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-import { data, movieDetail} from "./data.js";
+import { data, movieDetail } from "./data.js"; // movieDetail, global var, for feature details
 import { Cards } from "./components/cards/Cards.jsx";
 import { SearchBox } from "./components/search-box/SearchBox.jsx";
 import { Logo } from "./components/logo/Logo.jsx";
 import { MovieDetails } from "./components/movie-details/MovieDetails.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+/* way 1 for details feature: global var */
+// function App() {
+//   // add state
+//   const [movieData, setMovieData] = useState(data);
+//   const [movieName, setMovieName] = useState("");
+//   const [filteredData, setFilteredData] = useState([]);
+
+//   // filter movies
+//   useEffect(() => {
+//     /* way 1: .match(), works, best way*/
+//     const myMovies = data.filter((item) =>
+//       item.Title.match(new RegExp(movieName, "i"))
+//     ); //option 1: includes, works
+//     // const myMovies=data.filter(item => item.Title.match(new RegExp(`\\b${movieName}\\b`, 'i'))); // option 2: search word, works
+//     /* way 2: includes(), works*/
+//     // const myMovies = data.filter((item) =>
+//     //   item.Title.toLowerCase().includes(movieName.toLowerCase())
+//     // );
+//     /* way 3: indexOf(), works */
+//     //   const myMovies = data.filter((item) =>
+//     //   item.Title.toLowerCase().indexOf(movieName.toLowerCase()) !== -1
+//     // );
+
+//     console.log("myMovies:", myMovies);
+//     setFilteredData(myMovies);
+//   }, [movieName]);
+
+//   return (
+//     <Router>
+//       <div className="App">
+//         <Logo
+//           handleHome={() => {
+//             setMovieData(data);
+//             Object.keys(movieDetail).forEach((key) => delete movieDetail[key]);
+//             console.log("movieDetail:",movieDetail);
+//           }}
+//         />
+//         <SearchBox
+//           movieName={movieName}
+//           handleChange={(e) => setMovieName(e.target.value)}
+//           handleSearch={() => setMovieData(filteredData)}
+//         />
+//         <Routes>
+//           <Route path="/" element={<Cards data={movieData} />} />
+//           <Route
+//             exact
+//             path="/details"
+//             element={<MovieDetails movie={movieDetail} />}
+//           />
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+/* way 2 for details feature: name or value attribute, works, best way*/
 function App() {
   // add state
   const [movieData, setMovieData] = useState(data);
   const [movieName, setMovieName] = useState("");
   const [filteredData, setFilteredData] = useState([]);
+  const [myMovieDetail, setMyMovieDetail] = useState({}); // for details feature
 
   // filter movies
   useEffect(() => {
@@ -40,7 +99,7 @@ function App() {
           handleHome={() => {
             setMovieData(data);
             Object.keys(movieDetail).forEach((key) => delete movieDetail[key]);
-            console.log("movieDetail:",movieDetail);
+            console.log("movieDetail:", movieDetail);
           }}
         />
         <SearchBox
@@ -49,11 +108,24 @@ function App() {
           handleSearch={() => setMovieData(filteredData)}
         />
         <Routes>
-          <Route path="/" element={<Cards data={movieData} />} />
+          <Route
+            path="/"
+            element={
+              <Cards
+                data={movieData}
+                handleDetails={(e) => {
+                  console.log("you clicked me?:");
+                  const index= e.target.closest('button').value;
+                console.log("my-index:",index);
+                setMyMovieDetail(data[index]);
+                }}
+              />
+            }
+          />
           <Route
             exact
             path="/details"
-            element={<MovieDetails movie={movieDetail} />}
+            element={<MovieDetails movie={myMovieDetail} />}
           />
         </Routes>
       </div>
@@ -62,12 +134,3 @@ function App() {
 }
 
 export default App;
-
-/* 
-<Route
-            exact
-            path={`/${movieDetail.imdbID}`}
-            element={<MovieDetails movie={movieDetail} />}
-          />
-
-*/
